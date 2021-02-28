771edf6c0e857ba074ac59c17f0b1fb0624d2b4f
Mikhail Fogel <mikhail.fogel@renhealth.com>
2021-02-28 16:39:16 +0400

    Initial commit

--- ScrollViewTest/ViewController.swift
+++ ScrollViewTest/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        scrollView.delegate = self
+        rectRotateExample()
     }
 
-
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print("frame \(scrollView.layer.frame)")
+        print("bounds \(scrollView.layer.bounds)")
+    }
+    
+    func rectRotateExample() {
+        let frame = CGRect(x: 16, y: 400, width: 250, height: 30)
+        let myLabel = UILabel(frame: frame)
+        myLabel.backgroundColor = .systemIndigo
+        myLabel.textColor = .white
+        myLabel.textAlignment = .center
+        myLabel.text = "CGRect transform example"
+        view.addSubview(myLabel)
+        myLabel.transform = CGAffineTransform(rotationAngle: 45)
+    }
 }
-
